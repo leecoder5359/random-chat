e5e93b9f765142f56001b67cc17ef2c327121ae0
이호준 <leecoder5359@gmail.com>
2023-12-12 12:33:15 +0900

    (feat): client socket init

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,16 @@
-alert('hello world');
+const socket = io('/');
+const getElementById = (id) => document.getElementById(id) || null;
+
+const headerElement = getElementById('header');
+const chattingBoxElement = getElementById('chatting_box');
+const formElement = getElementById('chat_form');
+
+const helloUser = () => {
+  const userName = prompt('사용할 닉네임을 적어주세요.');
+};
+
+const init = () => {
+  helloUser();
+};
+
+init();
